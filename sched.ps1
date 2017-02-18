0e67399de6ce974b8710d6f7123ef6871921c72b
Dahlgren <jonas.dahlgren@saint-gobain.com>
2017-02-18 20:27:40 +0100

    ched

---
+++ sched.ps1
@@ -0,0 +1,7 @@
+$action = New-ScheduledTaskAction -Execute 'c:\users\j2017770\downloads\d42_winagent_x64.exe' 
+$DELAY = new-timespan -minutes 8
+$trigger =  New-ScheduledTaskTrigger -atlogon -randomdelay $DELAY
+$principal = New-ScheduledTaskPrincipal -UserId "nt authority\localservice" -LogonType ServiceAccount
+
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AppLog" -Description "D42 agent" -Principal $principal
