ff9795c5c0216f945c6a1f76f0e75b0432e95ff3
Dahlgren <jonas.dahlgren@saint-gobain.com>
2017-02-18 20:30:38 +0100

    me

--- sched.ps1
+++ sched.ps1
@@ -4,4 +4,4 @@ $trigger =  New-ScheduledTaskTrigger -atlogon -randomdelay $DELAY
 $principal = New-ScheduledTaskPrincipal -UserId "nt authority\localservice" -LogonType ServiceAccount
 
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AppLog" -Description "D42 agent" -Principal $principal
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "D42 Agent" -Description "D42 agent" -Principal $principal
