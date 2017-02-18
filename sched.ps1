67316ded45f31bf1f5d2011c97bfd3ee0c3c2d3a
Dahlgren <jonas.dahlgren@saint-gobain.com>
2017-02-18 20:57:27 +0100

    description

--- sched.ps1
+++ sched.ps1
@@ -1,7 +1,7 @@
 $action = New-ScheduledTaskAction -Execute 'c:\users\j2017770\downloads\d42_winagent_x64.exe' 
 $DELAY = new-timespan -minutes 8
-$trigger =  New-ScheduledTaskTrigger -atlogon -randomdelay $DELAY
+$trigger =  New-ScheduledTaskTrigger -atlogon -randomdelay $DELAY 
 $principal = New-ScheduledTaskPrincipal -UserId "system" -LogonType ServiceAccount
 
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "D42 Agent" -Description "D42 agent" -Principal $principal
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "D42 Agent" -Description "Device42 agent" -Principal $principal
